8665e06b661f9597c0b3813365b616b8b6e894df
Logan <32398465+loganDag@users.noreply.github.com>
2025-11-01 00:17:21 -0400

    Added things to files and made a new directory and file as well

--- backend/dbconnect.php
+++ backend/dbconnect.php
@@ -1,2 +1,3 @@
 <?php
+//Put MYsql Connection here when going live
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+require_once('./backend/dbconnect.php');
 require ('/bootstrap.html');
 
+//Insert phpmailer code here when pushed to server
+//Until then, use filler code and bypasses in order to see
+//what it would look like.
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ChristTube | Login</title>
+
+</head>
+<body>
+
+</body>
+</html>
